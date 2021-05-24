79472c5692f25fd325db9c09ef53bfeb81095071
yurakhomitsky <yura.khomitsky8@gmail.com>
2021-05-24 16:29:58 +0300

    React hooks

--- script.js
+++ script.js
@@ -2,6 +2,204 @@ import { Api } from './services/Api.js';
 import { VDom } from './services/VDom.js';
 // import { App } from './containers/App.js';
 
+const stream = {
+  subscribe(channel, listener) {
+    const id = channel.split('-')[1];
+    const normalizedId = id ? Number(id) : null;
+    if (!normalizedId) return;
+
+    const interval = setInterval(() => {
+      listener({
+        id: normalizedId,
+        price: Math.round(Math.random() * 10 + 30)
+      });
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }
+};
+
+const auctionInitialState = {
+  lots: [],
+  loading: false,
+  loaded: false,
+  error: null
+};
+
+const LOTS_LOADING_PENDING = 'LOTS_LOADING_PENDING';
+const LOTS_LOADING_SUCCESS = 'LOTS_LOADING_SUCCESS';
+const LOTS_LOADING_ERROR = 'LOTS_LOADING_ERROR';
+const CHANGE_LOT_PRICE = 'CHANGE_LOT_PRICE';
+const FAVORITE_LOT = 'FAVORITE_LOT';
+const UNFAVORITE_LOT = 'UNFAVORITE_LOT';
+const SET_LOADED = 'SET_LOADED';
+
+function auctionReducer(state = auctionInitialState, action) {
+  switch (action.type) {
+    case LOTS_LOADING_PENDING:
+      return {
+        ...state,
+        lots: [],
+        loading: true,
+        loaded: false,
+        error: null
+      };
+    case LOTS_LOADING_SUCCESS:
+      return {
+        ...state,
+        lots: action.lots,
+        loading: false,
+        loaded: true,
+        error: null
+      };
+    case LOTS_LOADING_ERROR:
+      return {
+        ...state,
+        lots: [],
+        loading: false,
+        loaded: false,
+        error: action.error
+      };
+    case CHANGE_LOT_PRICE:
+      return {
+        ...state,
+        lots: state.lots.map((lot) => {
+          if (lot.id === action.lot.id) {
+            return {
+              ...lot,
+              price: action.lot.price
+            };
+          }
+          return lot;
+        })
+      };
+    case FAVORITE_LOT:
+      return {
+        ...state,
+        lots: state.lots.map((lot) => {
+          if (lot.id === action.id) {
+            return {
+              ...lot,
+              isFavorite: true
+            };
+          }
+          return lot;
+        })
+      };
+    case UNFAVORITE_LOT:
+      return {
+        ...state,
+        lots: state.lots.map((lot) => {
+          if (lot.id === action.id) {
+            return {
+              ...lot,
+              isFavorite: false
+            };
+          }
+          return lot;
+        })
+      };
+    default: return state;
+  }
+}
+
+function lotsLoadingPending() {
+  return {
+    type: LOTS_LOADING_PENDING,
+  };
+}
+
+function lotsLoadingSuccess(lots) {
+  return {
+    type: LOTS_LOADING_SUCCESS,
+    lots
+  };
+}
+
+function lotsLoadingError(error) {
+  return {
+    type: LOTS_LOADING_ERROR,
+    error
+  };
+}
+
+function loadLots() {
+  return (dispatch, getState, { Api }) => {
+    dispatch(lotsLoadingPending());
+    Api.get('/lots')
+      .then((lots) => {
+        dispatch(lotsLoadingSuccess(lots));
+      })
+      .catch((error) => dispatch(lotsLoadingError(error.message)));
+  };
+}
+
+function changeLotPrice(lot) {
+  return {
+    type: CHANGE_LOT_PRICE,
+    lot
+  };
+}
+
+function favoriteLot(id) {
+  return {
+    type: FAVORITE_LOT,
+    id
+  };
+}
+
+function unFavoriteLot(id) {
+  return {
+    type: UNFAVORITE_LOT,
+    id
+  };
+}
+
+function favoriteLotAsync(id) {
+  return (dispatch, getState, { Api }) => {
+    Api.post(`/lots/${id}/favorite`).then(() => {
+      dispatch(favoriteLot(id));
+    });
+  };
+}
+
+function unFavoriteAsync(id) {
+  return (dispatch, getState, { Api }) => {
+    Api.post(`/lots/${id}/unfavorite`).then(() => {
+      dispatch(unFavoriteLot(id));
+    });
+  };
+}
+
+const thunk = ReduxThunk.default;
+
+const store = Redux.createStore(
+  Redux.combineReducers({
+    auction: auctionReducer
+  }),
+  Redux.applyMiddleware(thunk.withExtraArgument({ Api, stream }))
+);
+
+function lotsMapStateToProps(state) {
+  return {
+    lots: state.auction.lots,
+    loading: state.auction.loading,
+    loaded: state.auction.loaded,
+    error: state.auction.error
+  };
+}
+
+const lotMapDispatchToProps = {
+  favorite: favoriteLotAsync,
+  unFavorite: unFavoriteAsync,
+  subscribe: (id) => {
+    return (dispatch, getState, { stream }) => {
+      return stream.subscribe(`price-${id}`, (data) => {
+        dispatch(changeLotPrice({ id: data.id, price: data.price }));
+      });
+    };
+  }
+};
 
 /**
  * Returns loading component
@@ -59,7 +257,11 @@ export function Header() {
  * @param { object } { lot }
  * @return { HTMLElement } lot component
  */
-export function Lot({ lot, favorite, unFavorite }) {
+export function Lot({ lot, subscribe, favorite, unFavorite }) {
+
+  React.useEffect(() => {
+    return subscribe(lot.id);
+  }, [lot.id]);
 
   return (
     <article className={`lot ${lot.isFavorite ? 'favorite' : ''}`}>
@@ -83,9 +285,23 @@ function Favorite({ isFavorite, favorite, unFavorite }) {
  * @param { array } { lots }
  * @return { HTMLDivElement } list of lots components
  */
-export function Lots({ lots }) {
-  if (!lots || (Array.isArray(lots) && !lots.length)) {
+export function Lots({ lots, loading, loaded, error, loadLots }) {
+  React.useEffect(() => {
+    if (!loaded && !loading && !error) {
+      loadLots();
+    }
+  }, [loaded, loading, error]);
+
+  if (error) {
+    return 'Error!';
+  }
+
+  if (loading) {
     return <Loading />;
+  }
+
+  if (!loaded) {
+    return null;
   }
 
   return (
@@ -97,19 +313,7 @@ export function Lots({ lots }) {
   );
 }
 
-
-function lotsMapStateToProps(state) {
-  return {
-    lots: state.auction.lots
-  };
-}
-
-const lotMapDispatchToProps = {
-  favorite: favoriteLotAsync,
-  unFavorite: unFavoriteAsync
-};
-
-const LotsContainer = ReactRedux.connect(lotsMapStateToProps)(Lots);
+const LotsContainer = ReactRedux.connect(lotsMapStateToProps, { loadLots })(Lots);
 const LotContainer = ReactRedux.connect(null, lotMapDispatchToProps)(Lot);
 
 /**
@@ -127,139 +331,6 @@ export function App() {
   );
 }
 
-
-const stream = {
-  subscribe(channel, listener) {
-    const id = channel.split('-')[1];
-    const normalizedId = id ? Number(id) : null;
-
-    if (normalizedId) {
-      setInterval(() => {
-        listener({
-          id: normalizedId,
-          price: Math.round(Math.random() * 10 + 30)
-        });
-      }, 2000);
-    }
-  }
-};
-;
-
-const auctionInitialState = {
-  lots: null
-};
-
-const SET_LOTS = 'SET_LOTS';
-const CHANGE_LOT_PRICE = 'CHANGE_LOT_PRICE';
-const FAVORITE_LOT = 'FAVORITE_LOT';
-const UNFAVORITE_LOT = 'UNFAVORITE_LOT';
-
-
-function auctionReducer(state = auctionInitialState, action) {
-
-  switch (action.type) {
-    case SET_LOTS:
-      return {
-        ...state,
-        lots: action.lots
-      };
-    case CHANGE_LOT_PRICE:
-      return {
-        ...state,
-        lots: state.lots.map((lot) => {
-          if (lot.id === action.lot.id) {
-            return {
-              ...lot,
-              price: action.lot.price
-            };
-          }
-          return lot;
-        })
-      };
-    case FAVORITE_LOT:
-      return {
-        ...state,
-        lots: state.lots.map((lot) => {
-          if (lot.id === action.id) {
-            return {
-              ...lot,
-              isFavorite: true
-            };
-          }
-          return lot;
-        })
-      };
-    case UNFAVORITE_LOT:
-      return {
-        ...state,
-        lots: state.lots.map((lot) => {
-          if (lot.id === action.id) {
-            return {
-              ...lot,
-              isFavorite: false
-            };
-          }
-          return lot;
-        })
-      };
-    default: return state;
-  }
-}
-
-function setLots(lots) {
-  return {
-    type: SET_LOTS,
-    lots
-  };
-}
-
-function changeLotPrice(lot) {
-  return {
-    type: CHANGE_LOT_PRICE,
-    lot
-  };
-}
-
-function favoriteLot(id) {
-  return {
-    type: FAVORITE_LOT,
-    id
-  };
-}
-
-function unFavoriteLot(id) {
-  return {
-    type: UNFAVORITE_LOT,
-    id
-  };
-}
-
-function favoriteLotAsync(id) {
-  return (dispatch, getState, Api) => {
-    Api.post(`/lots/${id}/favorite`).then(() => {
-      dispatch(favoriteLot(id));
-    });
-  };
-}
-
-function unFavoriteAsync(id) {
-  return (dispatch, getState, api) => {
-    api.post(`/lots/${id}/unfavorite`).then(() => {
-      dispatch(unFavoriteLot(id));
-    });
-  };
-}
-
-const thunk = ReduxThunk.default;
-
-const store = Redux.createStore(
-  Redux.combineReducers({
-    auction: auctionReducer
-  }),
-  Redux.applyMiddleware(thunk.withExtraArgument(Api))
-);
-
-
 function renderView(store) {
   ReactDOM.render(
     <ReactRedux.Provider store={store}>
@@ -270,15 +341,3 @@ function renderView(store) {
 
 // Init app
 renderView(store);
-
-
-// Fetch lots
-Api.get('/lots').then((lots) => {
-  store.dispatch(setLots(lots));
-  // Simulate web-socket
-  lots.forEach((lot) => {
-    stream.subscribe(`price-${lot.id}`, (data) => {
-      store.dispatch(changeLotPrice({ id: data.id, price: data.price }));
-    });
-  });
-});
